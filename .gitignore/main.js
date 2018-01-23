dcd566d53b140301a84904dd01a8b0fd964065b6
xewup <35662288+xewup@users.noreply.github.com>
2018-01-23 17:12:44 +0100

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const kirby = new Discord.Client();
+const paul = new Discord.Client();
+
+kirby.login("Mzg0NzkzNjkwNDE5NDk0OTMy.DUUumA.cVam-1ZsawNxKHXi6WzZD6YV35g");
+paul.login("Mzg0Nzk3MDQyNDc3ODkxNTg0.DUU1TQ.2U84LXbB_7CkPC56FwpyJRS0A3Y");
+
+kirby.on('ready', function() {
+  console.log(` Kirby Prêt !`);
+  kirby.user.setGame("manger tout ce qu'il trouve");
+});
+
+paul.on('ready', function() {
+  console.log(`Paul Prêt !`);
+  paul.user.setGame("chasser le dino");
+  
+});
+
+paul.on('message', message => {
+  if(message.content === "Mais" ) {
+         message.channel.sendMessage("Oui car il y a toujours un mais.");
+  }
+});
+
+kirby.on('message', message => {
+  if(message.content === "Oui car il y a toujours un mais." ) {
+         message.channel.sendMessage("Surtout au mois de Mai.");
+  }
+});
+
+kirby.on("guildMemberAdd", function(member) {
+    let role = member.guild.roles.find("name", "Abonnés");
+    member.guild.channels.find("name","general").send(`Bienvenue sur le serveur ${member.user.username}. N'Hésite pas à faire un tour sur les #regles et à te présenter dans #presentations.`);
+    member.addRole(role);
+    });
+
+
+paul.on("guildMemberRemove", function(member) {
+     member.guild.channels.find("name","general").send(`Au revoir ${member.user.username} ! Encore un qui quitte le serveur.. Kirby tu pues trop de la bouche !`);
+      });

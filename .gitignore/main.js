87e78c83cd1f3104d2f51a177a884ab158498e1a
xewup <35662288+xewup@users.noreply.github.com>
2018-02-04 12:44:16 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -5,6 +5,7 @@ const paul = new Discord.Client();
 kirby.login("Mzg0NzkzNjkwNDE5NDk0OTMy.DUUumA.cVam-1ZsawNxKHXi6WzZD6YV35g");
 paul.login("Mzg0Nzk3MDQyNDc3ODkxNTg0.DUU1TQ.2U84LXbB_7CkPC56FwpyJRS0A3Y");
 
+// Démarrage.
 kirby.on('ready', function() {
   console.log(` Kirby Prêt !`);
   kirby.user.setGame("manger tout ce qu'il trouve");
@@ -16,6 +17,7 @@ paul.on('ready', function() {
   
 });
 
+// Surtout au mois de mai !
 paul.on('message', message => {
   if(message.content === "Mais" ) {
          message.channel.sendMessage("Oui car il y a toujours un mais.");
@@ -28,6 +30,16 @@ kirby.on('message', message => {
   }
 });
 
+  // la danse de kirby !
+kirby.on('message', message => {
+  if(message.content === "@Kirby, peux-tu danser s'il te plait ?" ) {
+         message.channel.sendMessage("(>^^)> <(^^<)");
+         message.channel.sendMessage("v(^-^)v v(^-^)^ ^(^-^)v");
+         message.channel.sendMessage("^(^-^)^");
+  }
+});
+
+// Un membre arrive.
 kirby.on("guildMemberAdd", function(member) {
     let role = member.guild.roles.find("name", "Abonnés");
     member.guild.channels.find("name","general").send(`Bienvenue sur le serveur ${member.user.username}. N'Hésite pas à faire un tour sur les #regles et à te présenter dans #presentations.`);
@@ -35,6 +47,7 @@ kirby.on("guildMemberAdd", function(member) {
     });
 
 
+// Un membre part.
 paul.on("guildMemberRemove", function(member) {
      member.guild.channels.find("name","general").send(`Au revoir ${member.user.username} ! Encore un qui quitte le serveur.. Kirby tu pues trop de la bouche !`);
       });

b8bf283d7c2e6eed8e8c0d82bc73fff0490ee6e4
xewup <35662288+xewup@users.noreply.github.com>
2018-05-10 22:59:37 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -5,10 +5,41 @@ const paul = new Discord.Client();
 kirby.login("Mzg0NzkzNjkwNDE5NDk0OTMy.DUUumA.cVam-1ZsawNxKHXi6WzZD6YV35g");
 paul.login("Mzg0Nzk3MDQyNDc3ODkxNTg0.DUU1TQ.2U84LXbB_7CkPC56FwpyJRS0A3Y");
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
 var adapter = new LocalStorage('db')
 var db = low(adapter)
+const express = require('express')
+const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileAsync = require('lowdb/adapters/FileAsync')
+
+// Create server
+const app = express()
+app.use(bodyParser.json())
+
+// Create database instance and start server
+const adapter = new FileAsync('db.json')
+low(adapter)
+  .then(db => {
+    // Routes
+    // GET /posts/:id
+    app.get('/posts/:id', (req, res) => {
+      const post = db.get('posts')
+        .find({ id: req.params.id })
+        .value()
+
+      res.send(post)
+    })
+
+    // POST /posts
+    app.post('/posts', (req, res) => {
+      db.get('posts')
+        .push(req.body)
+        .last()
+        .assign({ id: Date.now().toString() })
+        .write()
+        .then(post => res.send(post))
+    })
+
 
 db.defaults({ histoires: [], xp: []}).write()
 
